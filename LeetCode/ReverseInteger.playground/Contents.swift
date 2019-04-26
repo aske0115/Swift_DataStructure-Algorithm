77bcd69997ced6f0eab0e09988d0b254ba7385f6
GeunHwa Lee <lghaske@gmail.com>
2019-04-26 14:59:07 +0900

    - add Reverse Integer by LeetCode

---
+++ LeetCode/ReverseInteger.playground/Contents.swift
@@ -0,0 +1,49 @@
+/*
+ Given a 32-bit signed integer, reverse digits of an integer.
+ 
+ Example 1:
+ 
+ Input: 123
+ Output: 321
+ Example 2:
+ 
+ Input: -123
+ Output: -321
+ Example 3:
+ 
+ Input: 120
+ Output: 21
+ Note:
+ Assume we are dealing with an environment which could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+*/
+
+import Foundation
+
+class Solution {
+    
+    //Using String Array.. simple
+    func reverse(_ x: Int) -> Int {
+        let negative:Int = x < 0 ? -1 : 1
+        
+        let reverseString:String = String(x * negative)
+        
+        let reverse = Int(reverseString.map{$0}.reduce("") {"\($1)" + $0})! * negative
+        
+        return (reverse < Int32.min || reverse > Int32.max) ? 0 : reverse
+    }
+    
+    //Using while
+    func reverse2(_ x: Int) -> Int {
+        
+        var origin = x
+        var reverse = 0
+        
+        while(origin != 0) {
+            let data = origin % 10
+            reverse = reverse * 10 + data
+            origin /= 10
+        }
+        
+        return (reverse < Int32.min || reverse > Int32.max) ? 0 : reverse
+    }
+}
